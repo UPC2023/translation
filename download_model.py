dd69ff761b83e335b011de66c2f9ae1eaba40239
UPC2023 <chenyawen200501@163.com>
2026-01-13 17:20:42 +0800

    add download

---
+++ download_model.py
@@ -0,0 +1,21 @@
+import os
+from huggingface_hub import snapshot_download
+
+# 可以用镜像站或官方 Hugging Face
+# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+print("--- 正在下载 Qwen2.5-0.5B-Instruct ---")
+
+# 只改这一段
+try:
+    snapshot_download(
+        repo_id='Qwen/Qwen2.5-0.5B-Instruct', 
+        local_dir='./Qwen2.5-0.5B',            
+        resume_download=True,
+        max_workers=8
+    )
+    print("--- 恭喜！下载成功！ ---")
+    
+except Exception as e:
+    print("\n!!! 下载失败 !!!")
+    print("错误信息:", e)
